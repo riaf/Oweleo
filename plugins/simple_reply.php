7b5e0cc3db1f74b74ca7f66990589528011947b1
Keisuke SATO <riaf@nequal.jp>
2010-05-24 11:40:25 +0900

    remove debug code

--- plugins/simple_reply.php
+++ plugins/simple_reply.php
@@ -2,7 +2,6 @@
 return (object) array(
     'pattern' => '/^(.+)$/',
     'on_privmsg' => function($m, array &$stacks = array(), $match) {
-        echo $match, PHP_EOL;
         list($prefix, $mes) = $m->params;
         if ($match == 'hello') {
             $stacks[] = array(
