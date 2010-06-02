126b40553ea1d4ccddf401f22bad0594f3e4e2e7
Keisuke SATO <riaf@nequal.jp>
2010-06-02 15:35:56 +0900

    デバッグ文埋め込んだままだったよ

--- plugins/fetch_url_title.php
+++ plugins/fetch_url_title.php
@@ -3,7 +3,6 @@ require_once 'HTTP/Request2.php';
 
 return new OweleoPlugin('/(s?https?:\/\/[-_\.!~\*\'\(\)a-zA-Z0-9;\/?:@&=\+$,%#]+)/', array(
     'on_privmsg' => function(Oweleo $oweleo, Net_IRC_Message $msg, $match) {
-        var_dump($match);
         list($prefix, $mes) = $msg->params();
         $request = new HTTP_Request2($match[1], HTTP_Request2::METHOD_GET);
         try {
